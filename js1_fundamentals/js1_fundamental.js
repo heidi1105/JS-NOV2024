d63b54dab9a2623a744f716557bc7b0d93e8622c
heidi1105 <heidi1105@gmail.com>
2024-10-21 15:08:27 -0600

    Update js1_fundamental.js

--- js1_fundamentals/js1_fundamental.js
+++ js1_fundamentals/js1_fundamental.js
@@ -75,8 +75,84 @@ const personCopy2 = {
 console.log(person);
 console.log(personCopy2);
 
-const animals2 = ["dragon", ...animals, "unicorn"]; // push wihtout .push
+const animals2 = ["dragon", ...animals, "unicorn"]; // push without .push
 console.log(animals)
 console.log(animals2)
 
 
+// ------------- arrow function ------------
+function sayHello(name){
+    console.log(`Hello, ${name}!`);  
+}
+
+const sayHello2 = function(name){
+    console.log(`Hello, ${name}!`);  
+}
+
+const sayHello3 = (name) => {
+    console.log(`Hello, ${name}!`);  
+}
+// if there are more than 1 parameter, () is required
+// if there is only 1 parameter, () is optional
+const sayHello4 = name => console.log(`Hello, ${name}!`); 
+
+const greetAnonymous = () => console.log("Hi");
+
+sayHello4("Pepper");
+sayHello4("Heidi");
+
+// a function that return something
+function changePrice(price, discount){
+    return price * discount;
+}
+
+// longhanded arrow function - whenever {}, need "return"
+const changePrice2 = (price, discount) =>{
+    return price * discount;
+}
+
+// short handed arrow function - implicit return (without return keyword)
+const changePrice3 = (price, discount) => (
+    price * discount 
+)
+
+const changePrice4 = (price, discount) => price * discount; 
+
+
+const newPrice = changePrice4(100, 0.9);
+console.log(newPrice);
+
+// ------------- ternary operator ------------
+const canAfford = (itemPrice, accountBalance) =>{
+    if(itemPrice > accountBalance) {
+        return "Cannot afford!"
+    }else {
+        return "Can afford!";
+    }
+}
+
+const canAfford2 = (itemPrice, accountBalance) =>{
+    return itemPrice > accountBalance?
+        "Cannot afford!":
+        "Can afford";
+}
+
+const canAfford3 = (itemPrice, accountBalance) => 
+    (itemPrice > accountBalance)?
+        "Cannot afford":
+        "Can afford";
+
+
+const myAccountBalance = 1000;
+const drone = 1001;
+const chair = 299;
+console.log(canAfford3(drone, myAccountBalance));
+console.log(canAfford3(chair, myAccountBalance));
+
+const rating = 9;
+
+rating>5?
+    console.log("This movie is great"):
+    console.log("");
+
+rating< 10 && rating>5 && console.log("This movie is great!");
