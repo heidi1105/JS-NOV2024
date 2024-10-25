a1743aba3b3d8e4b1cac2597d2c5dc3d007d054d
heidi1105 <heidi1105@gmail.com>
2024-10-25 08:57:42 -0600

    Create mergeSort.js

---
+++ algos/week1/mergeSort.js
@@ -0,0 +1,72 @@
+// merge
+const sortedA1 = [];
+const sortedB1 = [2];
+const expectedMerge1 = [2];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [2, 3, 4, 7];
+const sortedB3 = [ 3];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10];
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Efficiently merges two already sorted arrays into a new sorted array.
+ * Do not mutate the given arrays.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ */
+function merge(left = [], right = []) {
+    let i = 0
+    let j = 0
+    result = []
+
+    while(i < left.length && j < right.length){ // both i & j are available, we can compare and push
+        if(left[i] < right[j]){
+            result.push(left[i])
+            i++   
+        }
+        else{
+            result.push(right[j])
+            j++
+        }
+    }
+
+    while(i< left.length){ // i has not reached the end (j ended)
+        result.push(left[i]);
+        i++; 
+    }
+
+    while(j<right.length){ // j has not reached the end (i ended)
+        result.push(right[j]);
+        j++;
+    }
+    return result
+}
+
+
+// mergeSort
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expectedSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Creates a new sorted array based on the given nums being recursively split
+ * and merged.
+ * Best: O(n log(n)) linearithmic.
+ * Avg: O(n log(n)) linearithmic.
+ * Worst: O(n log(n)) linearithmic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} A New sorted array.
+ */
+function mergeSort(nums = []) {}
