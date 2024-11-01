4b64df6bbdc75b10b732489f1277fdaa32587019
heidi1105 <heidi1105@gmail.com>
2024-11-01 15:04:13 -0600

    Update EditPage.jsx

--- js4_fullstack/mern-demo/client/src/views/EditPage.jsx
+++ js4_fullstack/mern-demo/client/src/views/EditPage.jsx
@@ -12,82 +12,35 @@ import axios from "axios"
 // After posting the data, redirect to dashboard page: useNavigate
 
 const EditPage = () => {
-  const {id} = useParams();
+  // Get id from params: useParams (Like Details)
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [imgUrl, setImgUrl] = useState("");
-  const [price, setPrice] = useState(0);
-  const [isAvailable, setIsAvailable] = useState(true);
+  // Create all the state variables for each form input (Like Create)
 
-  const navigate = useNavigate();
+  // Instantiate navigate to be used later
 
   useEffect(()=>{
-    axios.get(`http://localhost:8000/api/products/${id}`)
-      .then(res=>{
-        console.log(res.data); // need to update later
-        const foundProduct = res.data;
-        setTitle(foundProduct.title);
-        setDescription(foundProduct.description);
-        setImgUrl(foundProduct.imgUrl);
-        setPrice(foundProduct.price);
-        setIsAvailable(foundProduct.isAvailable);
-      })
-      .catch(err=>console.log(err))
+    // Get the product data from API, and set the state variables for each form input
   }, [])
 
   const handleSubmit = (e)=>{
     e.preventDefault();
-    const updatedData = {title, description, imgUrl, price, isAvailable};
-    axios.put(`http://localhost:8000/api/products/${id}`, updatedData)
-      .then(res=>{
-        navigate("/");
-      })
-      .catch(err=>console.log(err))
+    // Send a PUT request with the form data to the backend  (Like Create)
+    // navigate to other pages if it is successful
   }
 
+  
+
   const handleDelete = ()=>{
-    axios.delete(`http://localhost:8000/api/products/${id}`)
-      .then(res=>{
-        navigate("/");
-      })
-      .catch(err=>console.log(err))
+    // Create handleDelete to delete product from the API, navigate to dashboard if successful
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title</label>
-          <input type='text' name="title" value={title} className='form-control'
-              onChange={e=>setTitle(e.target.value)} />
-        </div>
-        <div>
-          <label>Image URL</label>
-          <input type='text' name="imgUrl" value={imgUrl} className='form-control'
-              onChange={e=>setImgUrl(e.target.value)} />
-        </div>
-        <div>
-          <label>Price</label>
-          <input type='number' name="price" value={price} className='form-control'
-              onChange={e=>setPrice(e.target.value)} />
-        </div>
-        <div>
-          <label>Description</label>
-          <textarea name='description' 
-            value={description} 
-            onChange={e=>setDescription(e.target.value)}
-            className='form-control'></textarea>
-        </div>
-        <div>
-          <label>Available?</label>
-          <input type="checkbox" name="isAvailable" checked={isAvailable}
-            onChange={e=>setIsAvailable(e.target.checked)} />
-        </div>
-        <button type='submit' className='btn btn-primary'>Submit</button>
-        <button type='button' onClick={e=>handleDelete()} className='btn btn-danger'>Delete</button>
-      </form>
-        
+      {/* 
+         Same as CreatePage, make sure all input have value referencing the state variable 
+         For submit button, any button will work, no onClick is needed
+         For Delete button, make sure it's either outside the form, OR have type="button" with onClick  
+      */}
     </div>
   )
 }
