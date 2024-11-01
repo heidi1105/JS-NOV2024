1fd16b67d073603a5aff5dc63e1f204f3859d97c
heidi1105 <heidi1105@gmail.com>
2024-11-01 11:33:31 -0600

    Update DetailsPage.jsx

--- js4_fullstack/mern-demo/client/src/views/DetailsPage.jsx
+++ js4_fullstack/mern-demo/client/src/views/DetailsPage.jsx
@@ -1,44 +1,23 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
-import axios from "axios"
-import { useEffect, useState } from "react"
+
 // Get id from params: useParams
 // use the ID to get data from API: axios
 // Display the info on load: useEffect
 // variable to store that data from API: useState
 
 const DetailsPage = () => {
-  const {id} = useParams();
-  const [product, setProduct] = useState();
+  // Get id from params: useParams
+  // Create state variable to store the product 
+
 
   useEffect(()=>{
-    axios.get(`http://localhost:8000/api/products/${id}`)
-      .then(res=>{
-        console.log(res.data);
-        setProduct(res.data);
-      })
-      .catch(err=>console.log(err))
+    // Axios to get the data from API (Postman route to get ONE)
+    // store the data in product 
   }, [])
   return (
     <div>
       {
-        product?
-        <div class="card">
-        <div class="row g-0">
-          <div class="col-5 col-sm-4">
-            <img src={product.imgUrl} class="img-fluid w-100" alt="card-horizontal-image"/>
-          </div>
-          <div class="col-7 col-sm-8">
-            <div class="card-body">
-              <h5 class="card-title">{product.title}</h5>
-              <p class="card-text">{product.description}.</p>
-              <p class="card-text">{product.price}</p>
-              <p class="card-text">Available: {product.isAvailable? "Yes": "No"}</p>
-            </div>
-          </div>
-        </div>
-      </div>:
-      <h3>Product is not available to show</h3>
+        /* DISPLAY THE DETAILS IF PRODUCT EXISTS */
       }
       
     </div>
