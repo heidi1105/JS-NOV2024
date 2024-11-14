72e6ead9219de1345eb4a6a7a0698ad6cf80bee9
heidi1105 <heidi1105@gmail.com>
2024-11-14 09:02:42 -0700

    Create w4d4.js

---
+++ algos/week4/w4d4.js
@@ -0,0 +1,23 @@
+
+/* 
+Given by Riot games.
+*/
+// b70a164c32a20c10
+
+// b 70
+// a 184
+// c 42
+// isNaN()
+
+const str1 = "b70a164c32a20c10";
+const expected1 = "a184b70c42";
+
+/**
+ * Rehashes an incorrectly hashed string by combining letter count occurrences
+ * and alphabetizing them.
+ * Time: O(?).
+ * Space: O(?).
+ * @param {string} s An incorrectly hashed string.
+ * @returns {string} The correctly rehashed string alphabetized.
+ */
+function rehash(s) {}
